cf985a235783c8e4a3c9be375fbcca53bf20b7d5
Gil W <gino.ladowitch@gmail.com>
2019-09-10 17:43:40 +0200

    Blog listing done.

--- footer.php
+++ footer.php
@@ -1,6 +1,3 @@
-</div>
-<?php // close .page ?>
-
 <footer>
 	<a href="">Contact</a> | 1 Cour du Havre 75008 Paris | 01 85 09 60 77 | <a href="#">Mentions légales</a><br>
 	Site réalisé par <a href="https://lautretribu.com">l'Autre Tribu</a>
--- header.php
+++ header.php
@@ -7,39 +7,23 @@
 	</head>
 
 <?php
-
 if (is_front_page()) {
 	echo '<body class="home">';
 }
 else {
 	echo '<body>';
 }
-
-// if (is_front_page()) {
-// 	echo '<div class="page home">';
-// }
-// else {
-// 	echo '<div id="page">';
-// }
-
 ?>
 
-<div class="logo">
-	<a href="https://nac1.gil-web.com/"><sup>+</sup>new<span>access</span>compta</a>
-</div>
-
-<p class="slogan">Donec consectetur a purus at venenatis</p>
+<header>
+	<div class="logo">
+		<?php echo '<a href="' . get_home_url() . '"><sup>+</sup>new<span>access</span>compta</a>'; ?>
+	</div>
+	<p class="slogan">Donec consectetur a purus at venenatis</p>
+</header>
 
 <?php
 
-// $site_title = get_bloginfo( 'name', 'display' );
-// $site_description = get_bloginfo( 'description', 'display' );
-
-// if ( function_exists( 'the_custom_logo' ) ) {
-//     the_custom_logo();
-// }
-// echo '<span>' . $site_title . '</span>';
-// echo '<span>' . $site_description . '</span>';
 echo '<a href="#" id="burger" class="menu-open"></a>';
 echo '<nav id="menu" class="menu-off">';
 echo '<a href="#" id="close" class="menu-close off">';
