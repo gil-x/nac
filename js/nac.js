88f4a816d40c0c6e0cbf4831c9d1716ff185cfde
gil <gino.ladowitch@gmail.com>
2019-09-29 14:02:01 +0200

    Upgraded menu.

--- js/nac.js
+++ js/nac.js
@@ -9,7 +9,20 @@ const menu = document.getElementById('menu');
 const burger = document.getElementById('burger');
 
 document.addEventListener("click", function(event) {
-    if (event.target.classList.contains('menu-open')) {
+    if (
+        event.target.parentNode.classList.contains('menu-item')
+    ) {
+        let childs = event.target.parentNode.childNodes;
+        for (let i = 0 ; i < childs.length ; i++) {
+            if (childs[i].tagName == 'UL') {
+                if (childs[i].classList.contains('sub-menu')) {
+                    document.querySelectorAll('.sub-menu').forEach(el => el.classList.remove('submenu-on'));
+                    childs[i].classList.toggle('submenu-on');
+                }
+            }
+        }
+    }
+    else if (event.target.classList.contains('menu-open')) {
         event.preventDefault();
         toggleMenu();
     }
