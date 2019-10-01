67b93330da6b13c24d06cdfb7f6a5a3e21692d47
gil <gino.ladowitch@gmail.com>
2019-10-01 17:14:29 +0200

    CSS & animations polish part1.

--- content-post.php
+++ content-post.php
@@ -3,10 +3,14 @@
 <h1><?php the_title(); ?></h1>
 
 
-<?php 
+<?php
 
-echo '<p class="excerpt">' . get_the_excerpt() . '</p>';
+echo '<div class="post-header">';
+echo '<p class="excerpt">' . get_the_excerpt() . '<br><span class="date">_<br>' . get_the_date() . '</span></p>';
 echo '<img class="thumbnail" src="' . get_the_post_thumbnail_url( $_post->ID, 'post-thumbnail' ) . '" alt="" />';
+
+echo '</div>';
+
 ?>
 
 <?php the_content(); ?>
