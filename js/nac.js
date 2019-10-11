bca29739bbf4d604bf89fc7b26b90457484f2054
gil <gino.ladowitch@gmail.com>
2019-10-11 16:24:11 +0200

    Fix: submenu now appears on hover.

--- js/nac.js
+++ js/nac.js
@@ -9,20 +9,7 @@ const menu = document.getElementById('menu');
 const burger = document.getElementById('burger');
 
 document.addEventListener("click", function(event) {
-    if (
-        event.target.parentNode.classList.contains('menu-item')
-    ) {
-        let childs = event.target.parentNode.childNodes;
-        for (let i = 0 ; i < childs.length ; i++) {
-            if (childs[i].tagName == 'UL') {
-                if (childs[i].classList.contains('sub-menu')) {
-                    document.querySelectorAll('.sub-menu').forEach(el => el.classList.remove('submenu-on'));
-                    childs[i].classList.toggle('submenu-on');
-                }
-            }
-        }
-    }
-    else if (event.target.classList.contains('menu-open')) {
+    if (event.target.classList.contains('menu-open')) {
         event.preventDefault();
         toggleMenu();
     }
@@ -37,3 +24,34 @@ document.addEventListener("click", function(event) {
         toggleMenu();
     }
 });
+
+
+// document.addEventListener("click", function(event) {
+//     if (
+//         event.target.parentNode.classList.contains('menu-item')
+//     ) {
+//         let childs = event.target.parentNode.childNodes;
+//         for (let i = 0 ; i < childs.length ; i++) {
+//             if (childs[i].tagName == 'UL') {
+//                 if (childs[i].classList.contains('sub-menu')) {
+//                     document.querySelectorAll('.sub-menu').forEach(el => el.classList.remove('submenu-on'));
+//                     childs[i].classList.toggle('submenu-on');
+//                 }
+//             }
+//         }
+//     }
+//     else if (event.target.classList.contains('menu-open')) {
+//         event.preventDefault();
+//         toggleMenu();
+//     }
+//     else if (event.target.classList.contains('menu-close')) {
+//         event.preventDefault();
+//         toggleMenu();
+//     }
+//     else if (
+//         menu.classList.contains('menu-on')
+//         && event.target.id != 'menu'
+//         && event.target.tagName != 'LI') {
+//         toggleMenu();
+//     }
+// });
