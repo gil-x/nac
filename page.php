984610218136768a036931416be47887f9912b10
gil <gino.ladowitch@gmail.com>
2019-10-27 23:30:48 +0100

    CSS for wp thumbnails.

--- page.php
+++ page.php
@@ -12,7 +12,7 @@ if( have_posts() ):
 	while( have_posts() ): the_post();
 		echo '<div class="content">';
 		if (has_post_thumbnail($_post->ID)) {
-			echo '<img src="' . get_the_post_thumbnail_url( $_post->ID, 'post-thumbnail' ) . '" alt="" />';
+			echo '<img class="picture" src="' . get_the_post_thumbnail_url( $_post->ID, 'post-thumbnail' ) . '" alt="" />';
 		}
 		the_content();
 		echo '</div>';
