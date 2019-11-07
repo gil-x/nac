43508a5b7c07bc7f6ea42acf9a11830eb17dfd83
gil <gino.ladowitch@gmail.com>
2019-11-07 09:59:33 +0100

    Added mixion for categories sidebar.

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<!-- <div id="go-up">
+	<div>(remonter)</div>
+</div> -->
+
 <footer>
 	<a href="<?php get_site_url() ?>/contact/">Contact</a> | 1&nbsp;Cour&nbsp;du&nbsp;Havre&nbsp;75008&nbsp;Paris | 01&nbsp;85&nbsp;09&nbsp;60&nbsp;77 | <a href="<?php get_site_url() ?>/mentions-legales/">Mentions&nbsp;légales</a><br>
 	<div class="copyright">Site&nbsp;réalisé&nbsp;par&nbsp;<a href="https://lautretribu.com">l'Autre&nbsp;Tribu</a></div>
