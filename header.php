a131f73dcc32ddc70f9aaca76bb432994795b871
gil <gino.ladowitch@gmail.com>
2019-12-18 14:16:47 +0100

    Added magic meta in header.php.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php the_title_attribute() ?></title>
 		<?php wp_head(); ?>
 	</head>
